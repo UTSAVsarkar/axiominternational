3e9440d38fa44707dff55b976a9ea04f4e8e9f07
UTSAVsarkar <utsavsarkar2000@gmail.com>
2026-05-07 21:57:47 +0530

    removed element

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import Button from "@mui/material/Button";
